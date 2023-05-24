6ee96ffdbba2692f4fe1d96a790e9ed4e0e8ef17
luoyikun <1806867003@qq.com>
2023-05-25 00:51:52 +0800

    字符串替换空格

---
+++ CSharpLeetCode/CSharpLeetCode/For/StringReplaceBank.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLeetCode.For
+{
+    //https://blog.algomooc.com/005.html
+    public static class StringReplaceBank
+    {
+        static string m_strOri = "We are happy";
+
+
+        public static string ReplaceBlank()
+        {
+            int oriLen = m_strOri.Length;
+            int i = 0;
+            int numBlank = 0;
+            while (i < oriLen)
+            {
+                if (m_strOri[i] == ' ')
+                {
+                    numBlank++;
+                }
+                i++;
+            }
+            int newLen = oriLen + numBlank * 2;
+            int idxOri = oriLen-1;
+            int idxNew = newLen -1;
+            char[] newStr = new char[newLen];
+            while (idxOri >= 0)
+            {
+                if (m_strOri[idxOri] == ' ')
+                {
+                    newStr[idxNew--] = '0';
+                    newStr[idxNew--] = '2';
+                    newStr[idxNew--] = '/';
+                }
+                else
+                {
+                    newStr[idxNew--] = m_strOri[idxOri];
+                }
+                idxOri--;
+            }
+            string strNew = new string(newStr);
+            Console.WriteLine(strNew);
+            return strNew;
+        }
+
+    }
+}
---
+++ CSharpLeetCode/CSharpLeetCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSharpLeetCode.For;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringReplaceBank.ReplaceBlank();
+            Console.ReadKey();
+        }
+    }
+}
