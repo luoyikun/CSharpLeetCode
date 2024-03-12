b8942d25c9b84bdfb4cd525b742b614e5020d662
luoyikun <1806867003@qq.com>
2024-03-12 22:39:04 +0800

    fibonacci数列

---
+++ CSharpLeetCode/CSharpLeetCode/Recursion/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLeetCode.Recursion
+{
+    public class Fibonacci
+    {
+        int fibonacci(int first, int second, int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+            if (n < 3)
+            {
+                return 1;
+            }
+            else if (n == 3)
+            {
+                return first + second;
+            }
+            else
+            {
+                return fibonacci(second, first + second, n - 1);
+            }
+        }
+    }
+}
